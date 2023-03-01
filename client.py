1f2ea2c348eb681733430ce257fe5fddbd4bb829
João Victor da Silva <93406483+Jotaveesse@users.noreply.github.com>
2023-03-01 11:37:21 -0300

    Mudanças na punição, parâmetros e prints

--- client.py
+++ client.py
@@ -19,12 +19,12 @@ def load_table(location):
 
 def load_empty_table():
     row = [0.0] * len(ACTIONS)
-    table = [row]*96
+    table = [row] * 96
     table = numpy.array(table)
     return table
 
 #aplcia a função de Q_learning e atualiza na tabela
-def update_table(reward, prev_s, prev_a, curr_s, learning_rate = 0.1, discount = 0.4):
+def update_table(reward, prev_s, prev_a, curr_s, learning_rate = 0.2, discount = 0.9):
     bellman = reward + discount * max(Q_TABLE[curr_s])
 
     Q_TABLE[prev_s][prev_a] += learning_rate * (bellman - Q_TABLE[prev_s][prev_a])
@@ -39,14 +39,15 @@ def extract_state(bit_state):
     platform = (int_state & plat_mask) >> 2
     direction = (int_state & direction_mask)
 
-    print(bit_state, platform, direction)
+    print('Bits: '+ bit_state + '\nPlataforma: ' + str(platform) + '\nDireção: ' + str(direction) + '\n')
+
     #acha o estado entre 0-95 a partir da direção e plataforma
     state = platform * 4 + direction
     return state
     
 def navigate(socket):
     #reseta o estado atual
-    current_state=0
+    current_state = 0
     
     while True:
         #busca na tabela a melhor ação pra esse estado
@@ -56,11 +57,10 @@ def navigate(socket):
         bit_state, reward = cn.get_state_reward(socket, ACTIONS[current_action])
         new_state = extract_state(bit_state)
 
-        print(new_state)
         current_state = new_state
 
-        #print_map(current_state)
-        print(ACTIONS[current_action])
+        print('Ação feita: ' + ACTIONS[current_action])
+        print('Novo estado: ' + str(new_state) + '\n')
         
         #para de navegar se input for nao vazio
         stop = input()
@@ -73,22 +73,25 @@ def explore(socket, times, start):
     current_state = start * 4
 
     for i in range(times):
-        print(i)
-        #busca na tabela a melhor ação pra esse estado e uma ação aleatoria
+        print('Passo: ' +  str(i))
+
+        #busca na tabela a melhor ação o estado atual
         best_action = numpy.where(Q_TABLE[current_state] == max(Q_TABLE[current_state]))[0][0]
         rand_action = random.randint(0, 2)
 
-        #escolhe entre uma ação aleatório ou a melhor ação aleatoriamente a partir dos pesos
-        if times>50:
-            weights = [i % (times/10), (times -1 - i) % (times/10)]
+        #escolhe entre uma ação aleatória ou a melhor ação
+        if i % 5 > 0:
+            current_action = best_action
         else:
-            weights = [i, times - i]
+            current_action = rand_action
 
-        current_action = random.choices([best_action, rand_action], weights)[0]
-
-        #recebe a recompensa e o estado novos a aprter da ação escolhida
+        #recebe a recompensa e o estado novos a partir da ação escolhida
         bit_state, reward  = cn.get_state_reward(socket, ACTIONS[current_action])
         new_state = extract_state(bit_state)
+
+        #diminui a punicao caso caia da plataforma
+        if reward == -100: 
+            reward = -14
 
         update_table(reward, current_state, current_action, new_state)
 
