12a44c98d2e1da9ecc0b4dcdbbd7b656e2c02370
João Victor da Silva <93406483+Jotaveesse@users.noreply.github.com>
2023-03-08 09:47:31 -0300

    fix: adição de comentários e explicação dos comandos
    comando "save best actions" removido, agora essa ação é feita dentro do comando save

--- client.py
+++ client.py
@@ -10,14 +10,17 @@ def save_table(location, table):
     numpy.savetxt(location, table)
 
 def save_best_action(location, table):
-    #salva a melhor ação para cada estado a partir da Q_table carregada
+    """
+    Salva a melhor ação para cada estado a partir da Q_table carregada
+    """
     new_table = [numpy.argmax(row) for row in table]
     best_actions = [ACTIONS[index] for index in new_table]
     numpy.savetxt(location, best_actions, fmt='%s')
-    print("saved!")
-    
+
 def compare_tables(best_actions, desired_best_actions):
-    #compara a tabela de best actions salva com a tabela de desired best actions
+    """
+    Compara a tabela de best actions salva com a tabela de desired best actions
+    """
     try:
         with open(desired_best_actions, 'r') as file:
             lines = file.readlines()
@@ -44,7 +47,8 @@ def compare_tables(best_actions, desired_best_actions):
             print("Arquivo best actions não encontrado")
     except FileNotFoundError:
         print("Arquivo desired actions não encontrado")
-        
+
+
 def load_table(location):
     try:
         with open(location) as file:
@@ -63,14 +67,34 @@ def load_empty_table():
     table = numpy.array(table)
     return table
 
-#aplcia a função de Q_learning e atualiza na tabela
-def update_table(reward, prev_s, prev_a, curr_s, learning_rate = 0.2, discount = 0.9):
+def update_table(reward: int, prev_s: int, prev_a: int, curr_s: int, learning_rate: float = 0.2, discount: float = 0.9):
+    """
+    Atualiza a a tabela usando a equação de atualização
+
+    Parâmetros:
+    reward (int): a recompesna recebida
+    prev_s (int): o estado anterior
+    prev_a (int): a ação realizada
+    curr_s (int): o estado atual
+    learning_rate (int): o valor do learning rate
+    discount (int): o valor do desconto
+    """
     bellman = reward + discount * max(Q_TABLE[curr_s])
 
+    #equação de atualização
     Q_TABLE[prev_s][prev_a] += learning_rate * (bellman - Q_TABLE[prev_s][prev_a])
     return Q_TABLE[prev_s][prev_a]
 
-def extract_state(bit_state):
+def extract_state(bit_state: str):
+    """
+    Extrai o estado a partir do valor binário
+
+    Parâmetros:
+    bit_state (str): valor binário que representa a plataforma e direção
+    
+    Retorna:
+    state (int): estado extraido
+    """
     plat_mask = 0b1111100
     direction_mask = 0b0000011
 
@@ -79,17 +103,24 @@ def extract_state(bit_state):
     platform = (int_state & plat_mask) >> 2
     direction = (int_state & direction_mask)
 
-    print(f"Bits: {bit_state}  \nPlataforma:  {platform} \nDireção: {DIRECTIONS[direction]}\n")
-
     #acha o estado entre 0-95 a partir da direção e plataforma
     state = platform * 4 + direction
     return state
     
-def navigate(socket):
+def navigate(socket, times: int, start: int):
+    """
+    Navega pelo mapa usando a melhor ação de cada estado
+
+    Parâmetros:
+    socket: socket para a conexão
+    times (int): quantidade de passos que irão ser feitos
+    start (int): plataforma inicial
+    """
     #reseta o estado atual
-    current_state = 0
-    
-    while True:
+    current_state = start * 4
+    total_reward = 0
+
+    for i in range(times):
         #busca na tabela a melhor ação pra esse estado
         current_action = numpy.where(Q_TABLE[current_state] == max(Q_TABLE[current_state]))[0][0]
 
@@ -97,18 +128,23 @@ def navigate(socket):
         bit_state, reward = cn.get_state_reward(socket, ACTIONS[current_action])
         new_state = extract_state(bit_state)
 
+        total_reward += reward
         current_state = new_state
 
         print('Ação feita: ' + ACTIONS[current_action])
-        print('Novo estado: ' + str(new_state) + '\n')
-        
-        #para de navegar se input for nao vazio
-        stop = input()
-        if(stop!=''):
-            break
+        print(f'Novo estado: {new_state}')
+        print(f'Recompensa média: {round(total_reward/(i+1), 3)} \n')
 
 
-def explore(socket, times, start):
+def explore(socket, times: int, start: int):
+    """
+    Explora o mapa e atualiza a tabela com base na recompensa recebida
+
+    Parâmetros:
+    socket: socket para a conexão
+    times (int): quantidade de passos que irão ser feitos
+    start (int): plataforma inicial
+    """
     #reseta o estado atual
     current_state = start * 4 # *4, pois cada plataforma tem 4 estados possíveis e sempre respawna virado pro norte
 
@@ -122,13 +158,13 @@ def explore(socket, times, start):
         # ---------------- # ! Lógicas de exploration x exploitation (ambas foram usadas) # ---------------------
         # ? escolhe entre uma ação aleatório ou a melhor ação aleatoriamente a partir dos pesos
         # if times>50:
-        #     weights = [i % (times/10), (times -1 - i) % (times/10)]
+        # weights = [i % (times/10), (times -1 - i) % (times/10)]
         # else:
-        #     weights = [i, times - i]
+        # weights = [i, times - i]
         # current_action = random.choices([best_action, rand_action], weights)[0]
 
         # ? escolhe entre uma ação aleatória ou a melhor ação
-        if i % 5 > 0:
+        if i % 5 > 1:
             current_action = best_action
         else:
             current_action = rand_action
@@ -136,6 +172,7 @@ def explore(socket, times, start):
 
         #recebe a recompensa e o estado novos a partir da ação escolhida
         bit_state, reward  = cn.get_state_reward(socket, ACTIONS[current_action])
+
         new_state = extract_state(bit_state)
 
         update_table(reward, current_state, current_action, new_state)
@@ -144,42 +181,61 @@ def explore(socket, times, start):
 
 def main():
     global Q_TABLE
-    socket = cn.connect(2037)
 
+    socket = cn.connect(2037)
+    
     if(socket != 0):
+        print('\nDigite "help" para ver os comandos')
         while True:
-            command = input('Envie um comando\n')
+            command = input('\nInsira um comando\n')
             match command:
                 case 'save':
                     save_table('resultado.txt', Q_TABLE)
+                    save_best_action('best_actions.txt', Q_TABLE)
                     print('Tabela salva')
+
                 case 'empty':
                     Q_TABLE = load_empty_table()
                     print('Tabela vazia carregada')
+
                 case 'load':
                     Q_TABLE = load_table('resultado.txt')
                     print('Tabela carregada')
+
                 case 'explore':
                     if Q_TABLE is None:
                         print('Carregue uma tabela antes')
                     else:
-                        times = int(input('Quantas vezes ele deve explorar?\n'))
-                        start = int(input('Qual a plataforma inicial?\n'))
-                        explore(socket, times, start)
-                        print('Exploração terminada')
+                        try:
+                            times = int(input('Quantas vezes ele deve explorar?\n'))
+                            start = int(input('Qual a plataforma inicial?\n'))
+                        except ValueError:
+                            print('Valor inválido')
+                        else:
+                            explore(socket, times, start)
+                            print('Exploração terminada')
                 case 'navigate':
                     if Q_TABLE is None:
-                        print('Carregue uma tabela antes')
+                        print('Carregue uma Q_table antes')
                     else:
-                        print('Aperte Enter para avançar 1 passo, digite algo para terminar\n')
-                        navigate(socket)
-                case 'save best action':
-                    if Q_TABLE is None:
-                        print('Carregue uma tabela antes')
-                    else:
-                        save_best_action('best_actions.txt', Q_TABLE)
+                        try:
+                            times = int(input('Quantas vezes ele deve percorrer?\n'))
+                            start = int(input('Qual a plataforma inicial?\n'))
+                        except ValueError:
+                            print('Valor inválido')
+                        else:
+                            navigate(socket, times, start)
                 case 'compare tables':
                     compare_tables('best_actions.txt', 'desired_best_actions.txt')
+
+                case 'help':
+                    print('\nsave: Salva a Q_table e suas melhores ações para cada estado nos arquivos de texto\n')
+                    print('empty: Carrega uma Q_table vazia\n')
+                    print('load: Carrega a Q_table do arquivo de texto\n')
+                    print('explore: Explora o mapa e atualiza a Q_table a partir das recompensas\n')
+                    print('navigate: Navega o mapa usando a melhor ação de cada estado\n')
+                    print('compare tables: Compara as melhores ações salvas com as melhores ações desejadas\n')
+                    print('exit: Termina o programa')
                 case 'exit':
                     socket.close()
                     break
